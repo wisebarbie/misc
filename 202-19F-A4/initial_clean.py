dab337686ad28b178e609709665080746c91fe0d
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-12-01 04:51:37 -0500

    start 1.2

--- 202-19F-A4/initial_clean.py
+++ 202-19F-A4/initial_clean.py
@@ -34,5 +34,42 @@ def which_delimiter(string):
     raise AssertionError('Must contain at least one delimiter (space/comma/tab)')
 
 
+def stage_one(input_filename, output_filename):
+    '''
+    (str, str) -> int
+    Read input_filename and write to output_filename after making the following changes: 
+    1. Change the most common delimiter to tab (if it is not already tab-delimited) 
+    2. Change all text to be upper case
+    3. Change any / or . in the dates to hyphens (-) 
+    Return the number of lines written to output_filename
+    >>> stage_one('stage1.txt', 'stage1.tsv')
+    4
+    '''
+    # open relevant files
+    input_file = open(input_filename, 'r', encoding='utf-8')
+    output_file = open(output_filename, 'w', encoding='utf-8')
+    # store lines from input_filename as a list
+    input_lines = input_file.readlines()
+    # initialize variable to count number of lines written to output_filename
+    line_count = 0
+    # do for all lines
+    for line in input_lines:
+        # change the most common delimiter to tab 
+        line = line.replace(which_delimiter(line), '\t')
+        # change all text to be upper case
+        line = line.upper()
+        # change any / or . in the dates to hyphens (-) 
+        line = line.replace('.', '-')
+        line = line.replace('/', '-')
+        # write line to output_filename and increment line count
+        output_file.write(line)
+        line_count += 1 
+    # close relevant files
+    input_file.close()
+    output_file.close()
+    # return line count
+    return line_count
+
+
 if __name__ == '__main__':
     doctest.testmod()
