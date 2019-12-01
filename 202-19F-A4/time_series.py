54320b0ccb9ad637b346eb998c86b0e7f54abf77
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-12-01 05:56:58 -0500

    finish 2.1

--- 202-19F-A4/time_series.py
+++ 202-19F-A4/time_series.py
@@ -4,5 +4,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def date_diff(date1, date2):
+    '''
+    (str, str) -> int
+    Take dates formatted in ISO format
+    Return number of days apart the two dates are
+    Note that that date can be positive/negative:
+    - if the first date is earlier than the second date, the number should be positive;
+    - if the second date is earlier than the first date, the number should be positive;
+    >>> date_diff('2019-10-31', '2019-11-2') 
+    2
+    >>> date_diff('2020-5-3', '2020-6-30') 
+    58
+    >>> date_diff('2019-12-31', '2019-11-27') 
+    -34
+    '''
+    # convert date from string to integer given its specified ISO format 
+    date1 = datetime.strptime(date1, '%Y-%m-%d')
+    date2 = datetime.strptime(date2, '%Y-%m-%d')
+    # compute difference between dates
+    time_delta = date2 - date1
+    # return difference as number of days
+    return time_delta.days
+
+
 if __name__ == '__main__':
     doctest.testmod()
